8098d8e3cfff279cad2c14e918fe791f139b9c80
foster154 <foster154@gmail.com>
2014-04-21 07:35:23 -0600

    Begin show tour work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417043443) do
+ActiveRecord::Schema.define(version: 20140420224633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140417043443) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "position"
   end
 
   add_index "photos", ["tour_id"], name: "index_photos_on_tour_id", using: :btree
