c20f0760914f2ae5ce06f0954070c17f1f36eb32
foster154 <foster154@gmail.com>
2014-08-25 22:28:53 -0600

    troubleshooting

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   before_destroy :cancel_subscription
 
   def update_stripe
-  	# return if email.include?(ENV['ADMIN_EMAIL'])
+  	return if email.include?(ENV['ADMIN_EMAIL'])
     return if email.include?('@example.com') and not Rails.env.production?
     if customer_id.nil?
       if !stripe_token.present?
